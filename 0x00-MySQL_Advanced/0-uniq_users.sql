d7fc25f828571ab565c4784afeb98bf814f159c4
Emmyy882 <emmaagbaje3@gmail.com>
2024-03-22 10:56:48 +0100

    MYSQL advanced

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,5 @@
---SQL script that creates a table users
---with these attributes:
-------------id, integer, never null, auto increment and primart key
-------------email, string (255 character), never null and unique
-------------name, string (255 characters)
---if the table already exits, your script should not fail
+-- SQL script that creates a table users
+-- if the table already exits, your script should not fail
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
